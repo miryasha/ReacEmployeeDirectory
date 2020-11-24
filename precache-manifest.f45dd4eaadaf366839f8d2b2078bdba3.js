595f6a85383052a0bd35733f389a9d966f0c1c64
yasha6081 <yasha6081@gmail.con>
2020-11-23 22:54:38 -0600

    Updates

---
+++ precache-manifest.f45dd4eaadaf366839f8d2b2078bdba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87a3149819a6b8d2856727549995d2a",
+    "url": "/ReacEmployeeDirectory/index.html"
+  },
+  {
+    "revision": "0cb10f72c4f8c3e0a7de",
+    "url": "/ReacEmployeeDirectory/static/css/main.ed0771a9.chunk.css"
+  },
+  {
+    "revision": "f7633277ba890df0ef77",
+    "url": "/ReacEmployeeDirectory/static/js/2.805e8040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReacEmployeeDirectory/static/js/2.805e8040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb10f72c4f8c3e0a7de",
+    "url": "/ReacEmployeeDirectory/static/js/main.555c5cf0.chunk.js"
+  },
+  {
+    "revision": "55832d2e0952c7b586cd",
+    "url": "/ReacEmployeeDirectory/static/js/runtime-main.95e99394.js"
+  }
+]);
